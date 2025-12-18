1c86785bb6717b37c7dc258c7a05bcc40bb5339c
Stefan Karlsson <stefankarlsson1230@gmail.com>
2025-12-18 18:13:08 +0100

    Finished Part 2

--- C-Sharp OOP #2/C-Sharp OOP #2/Program.cs
+++ C-Sharp OOP #2/C-Sharp OOP #2/Program.cs
@@ -4,3 +4,8 @@ Student student = new("Totte", "Bengtsson", new DateTime(1974, 12, 30));
 
 Console.WriteLine(student);
 Console.WriteLine(student.GetAge());
+
+Teacher teacher = new("Bliniken", "Orvar", new DateTime(1972, 01, 02));
+
+Console.WriteLine(teacher);
+Console.WriteLine(teacher.GetAge());
